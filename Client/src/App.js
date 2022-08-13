8e4f900100750a74fe5fc67bd0c5a6a5024b8051
nehabhange <neha05bhange@gmail.com>
2022-08-13 21:28:02 +0530

    Sign Up Page

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,16 +5,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserType } from "./Pages";
 import About from "./components/Stepper/About";
 import Stage from "./components/Stepper/Stage";
-
+import SignUp from "./Pages/SignUp/index"
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/user-type" element={<UserType />} />
-				<Route exact path="/sign-up" element={<SignUpForm />} />
+				<Route exact path="/sign-up" element={<SignUp/>} />
 				<Route exact path="/contact" element={<Contact />} />
 				<Route exact path="/about" element={<About />} />
 				<Route exact path="/stage" element={<Stage />} />
+
 			</Routes>
 		</div>
 	);
