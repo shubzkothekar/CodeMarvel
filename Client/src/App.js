59e336b5dea5a41010f46d7dce3e7382b033721e
Shubham Kothekar <shubzkothekar@Shubhams-MacBook-Air.local>
2022-08-13 19:34:47 +0530

    steeper

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import SignUpForm from './components/SignUp';
-import Contact from './components/SignUp/contact';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SignUp } from './Pages';
+import "./App.css";
+import SignUpForm from "./components/SignUp";
+import Contact from "./components/SignUp/contact";
+import { Routes, Route } from "react-router-dom";
+import { SignUp } from "./Pages";
+import Stage from "./components/Stepper/Stage";
 
 function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<SignUp />} />
+				<Route exact path="/sign-up" element={<SignUpForm />} />
+				<Route exact path="/contact" element={<Contact />} />
 
-  return (
-   
-    <div className="App">
-    <Routes>
-     <Route path="/" element={<SignUp/>}/>
-    <Route exact path="/sign-up" element={<SignUpForm/>}/>
-    <Route exact path="/contact" element={<Contact/>}/>
-    </Routes>
-    </div>
-   
-  );
+				<Route exact path="/stage" element={<Stage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
