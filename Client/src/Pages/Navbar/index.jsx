7d53fe5e6cf55bd572a0b6f467de4db323528c68
Shubham Kothekar <shubzkothekar@Shubhams-MacBook-Air.local>
2022-08-14 08:23:45 +0530

    bugfix

--- Client/src/Pages/Navbar/index.jsx
+++ Client/src/Pages/Navbar/index.jsx
@@ -5,42 +5,112 @@ import { Accessibility } from "./accessible";
 import { NavLinks } from "./navLinks";
 import { DeviceSize } from "./responsive";
 import { MobileNavLinks } from "./menuNavLinks";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const NavbarContainer = styled.div`
-  
-  height: 60px;
-  box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
-  display: flex;
-  align-items: center;
-  padding: 0 1.5em;
+	height: 60px;
+	box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
+	display: flex;
+	align-items: center;
+	padding: 0 1.5em;
 `;
 
 const LeftSection = styled.div`
-  display: flex;
+	display: flex;
 `;
 
 const MiddleSection = styled.div`
-  display: flex;
-  flex: 2;
-  height: 100%;
-  justify-content: center;
+	display: flex;
+	flex: 2;
+	height: 100%;
+	justify-content: center;
 `;
 
 const RightSection = styled.div`
-  display: flex;
+	display: flex;
 `;
 
 export function Navbar(props) {
-  const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
+	const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
+	const [data, setData] = React.useState("");
 
-  return (
-    <NavbarContainer>
-      
-      <MiddleSection>{!isMobile && <NavLinks />}</MiddleSection>
-      <RightSection>
-        {!isMobile && <Accessibility />}
-        {isMobile && <MobileNavLinks />}
-      </RightSection>
-    </NavbarContainer>
-  );
+	React.useEffect(() => {
+		if (
+			localStorage.getItem("menu") !== "null" &&
+			localStorage.getItem("menu") !== null
+		) {
+			axios
+				.post("http://localhost:3000/find", {
+					type: localStorage.getItem("menu"),
+				})
+				.then((res) => {
+					console.log(res);
+					if (res.data !== "") {
+						setData(res.data);
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			axios
+				.post("http://localhost:3000/find", {
+					type: "STARTUP",
+				})
+				.then((res) => {
+					console.log(res);
+					if (res.data !== "") {
+						setData(res.data);
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [localStorage.getItem("menu")]);
+	return (
+		<div>
+			<NavbarContainer>
+				<MiddleSection>{!isMobile && <NavLinks />}</MiddleSection>
+				<RightSection>
+					{!isMobile && <Accessibility />}
+					{isMobile && <MobileNavLinks />}
+				</RightSection>
+			</NavbarContainer>
+			<div style={{ margin: "5%" }}>
+				{data !== ""
+					? data.map((d) => (
+							<Card
+								onClick={() => {
+									window.location.href = `/user-profile/${d._id}`;
+								}}
+								sx={{ width: "30%" }}
+							>
+								<CardMedia
+									component="img"
+									height="210"
+									image={d.image}
+									alt="green iguana"
+								/>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="div">
+										{d.name}
+									</Typography>
+									<Typography variant="body2" color="text.secondary">
+										{d.about}
+									</Typography>
+									<br></br>
+								</CardContent>
+							</Card>
+					  ))
+					: ""}
+			</div>
+		</div>
+	);
 }
--- Client/src/Pages/Navbar/navLinks.jsx
+++ Client/src/Pages/Navbar/navLinks.jsx
@@ -2,65 +2,107 @@ import React from "react";
 import styled from "styled-components";
 
 const NavLinksContainer = styled.div`
-  height: 100%;
-  display: flex;
-  align-items: center;
+	height: 100%;
+	display: flex;
+	align-items: center;
 `;
 
 const LinksWrapper = styled.ul`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  height: 100%;
-  list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	height: 100%;
+	list-style: none;
 `;
 
 const LinkItem = styled.li`
-  height: 100%;
-  padding: 0 1.1em;
-  color: #222;
-  font-weight: 500;
-  font-size: 14px;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  border-top: 2px solid transparent;
-  transition: all 220ms ease-in-out;
+	height: 100%;
+	padding: 0 1.1em;
+	color: #222;
+	font-weight: 500;
+	font-size: 14px;
+	align-items: center;
+	justify-content: center;
+	display: flex;
+	border-top: 2px solid transparent;
+	transition: all 220ms ease-in-out;
 
-  &:hover {
-    border-bottom: 2px solid #FF725e;
-  }
+	&:hover {
+		border-bottom: 2px solid #ff725e;
+	}
 `;
 
 const Link = styled.a`
-  text-decoration: none;
-  color: inherit;
-  font-size: inherit;
+	text-decoration: none;
+	color: inherit;
+	font-size: inherit;
 `;
 
 export function NavLinks(props) {
-  return (
-    <NavLinksContainer>
-      <LinksWrapper>
-      <LinkItem>
-      <Link href="#">Start Up</Link>
-    </LinkItem>
-    <LinkItem>
-      <Link href="#">Investors</Link>
-    </LinkItem>
-    <LinkItem>
-      <Link href="#">Mentors</Link>
-    </LinkItem>
-    <LinkItem>
-      <Link href="#">Incubators</Link>
-    </LinkItem>
-    <LinkItem>
-      <Link href="#">Accelerators</Link>
-    </LinkItem>
-    <LinkItem>
-      <Link href="#">Corporate</Link>
-    </LinkItem>
-      </LinksWrapper>
-    </NavLinksContainer>
-  );
+	return (
+		<NavLinksContainer>
+			<LinksWrapper>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "STARTUP");
+							window.location.reload();
+						}}
+					>
+						Start Up
+					</Link>
+				</LinkItem>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "INVESTOR");
+							window.location.reload();
+						}}
+					>
+						Investors
+					</Link>
+				</LinkItem>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "MENTOR");
+							window.location.reload();
+						}}
+					>
+						Mentors
+					</Link>
+				</LinkItem>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "INCUBATOR");
+							window.location.reload();
+						}}
+					>
+						Incubators
+					</Link>
+				</LinkItem>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "ACCELERATOR");
+							window.location.reload();
+						}}
+					>
+						Accelerators
+					</Link>
+				</LinkItem>
+				<LinkItem>
+					<Link
+						onClick={() => {
+							localStorage.setItem("menu", "CORPORATE");
+							window.location.reload();
+						}}
+					>
+						Corporate
+					</Link>
+				</LinkItem>
+			</LinksWrapper>
+		</NavLinksContainer>
+	);
 }
