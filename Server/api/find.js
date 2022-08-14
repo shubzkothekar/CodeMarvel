6545aa25578db378b2ea0f824fe488fd9ca7ea21
Shubham Kothekar <shubzkothekar@Shubhams-MacBook-Air.local>
2022-08-14 08:23:59 +0530

    bugfix

--- Server/api/find.js
+++ Server/api/find.js
@@ -3,7 +3,11 @@ const { ObjectId } = require("mongodb");
 
 module.exports = async (req, res) => {
 	try {
-		let result = await client.db("codefury").collection("users").find(req.body);
+		let result = await client
+			.db("codefury")
+			.collection("users")
+			.find(req.body)
+			.toArray();
 		res.status(200).send(result);
 	} catch (error) {
 		res.json(error);
