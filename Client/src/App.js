a1d37cda5b98c375b3ae37b544c3a74ca0a48656
nehabhange <neha05bhange@gmail.com>
2022-08-14 05:47:11 +0530

    Created Chats Component

--- Client/src/App.js
+++ Client/src/App.js
@@ -9,6 +9,7 @@ import Login from "./Pages/Login/index";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import UserProfile from "./Pages/UserProfile/index"
 import { Navbar } from "./Pages/Navbar";
+import {Chats} from "./Pages/Chats"
 
 function App() {
 	return (
@@ -23,6 +24,7 @@ function App() {
 				<Route path="/" element={<LandingPage />} />
         <Route path="/user-profile" element={<UserProfile/>}/>
 		<Route path="/navbar" element={<Navbar/>}/>
+		<Route path="/chats" element={<Chats/>}/>
 			</Routes>
 		</div>
 	);
